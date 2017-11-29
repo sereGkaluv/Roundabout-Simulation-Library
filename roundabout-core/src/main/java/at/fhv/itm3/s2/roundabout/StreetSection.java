3773045d86bc8e0f302e7ce39b40c09c312f6e22
sereGkaluv <malicx7@gmail.com>
2017-11-29 17:42:27 +0100

    street section in core

---
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/StreetSection.java
@@ -0,0 +1,12 @@
+package at.fhv.itm3.s2.roundabout;
+
+import desmoj.core.simulator.Entity;
+import desmoj.core.simulator.Model;
+
+public class StreetSection extends Entity {
+
+
+    public StreetSection(Model model, String s, boolean b) {
+        super(model, s, b);
+    }
+}
