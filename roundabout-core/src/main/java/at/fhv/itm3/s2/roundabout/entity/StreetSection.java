66a074e84c8d1b83731fb49ede19eeac6c34c646
Karin <karin.trommelschlaeger@students.fhv.at>
2017-11-29 18:16:21 +0100

    add queue<Car>

--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
@@ -4,7 +4,12 @@ import at.fhv.itm3.s2.roundabout.api.entity.IStreetSection;
 import desmoj.core.simulator.Entity;
 import desmoj.core.simulator.Model;
 
+import java.util.Queue;
+
 public class StreetSection extends Entity implements IStreetSection {
+
+    private Queue<Car> queue;
+
 
 
     public StreetSection(Model model, String s, boolean b) {
@@ -25,5 +30,12 @@ public class StreetSection extends Entity implements IStreetSection {
 
     public void moveFirstCarToNextSection() {
 
+    }
+    public Queue<Car> getQueue() {
+        return queue;
+    }
+
+    public void setQueue(Queue<Car> queue) {
+        this.queue = queue;
     }
 }
