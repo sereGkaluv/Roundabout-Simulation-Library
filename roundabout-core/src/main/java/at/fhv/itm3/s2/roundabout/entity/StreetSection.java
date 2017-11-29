400b59f84e3fcd73e91d532f5982e46546e7b458
daniel <dgr7348@students.fhv.at>
2017-11-29 17:57:31 +0100

    init method

--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
@@ -5,8 +5,17 @@ import desmoj.core.simulator.Model;
 
 public class StreetSection extends Entity {
 
-
     public StreetSection(Model model, String s, boolean b) {
         super(model, s, b);
+    }
+
+    public boolean carCouldEnterNextSection() {
+        // car knows its route (next section)
+
+        // update position in next section
+        // if is enough space in next section
+        // update pos in previous roundabout section
+        // if no car entering from previous roundabout section && enough space in previous section
+        return false;
     }
 }
