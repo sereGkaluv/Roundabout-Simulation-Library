53155a3373de63f63334f6b8c63bea1e8454ad50
daniel <dgr7348@students.fhv.at>
2017-11-29 18:14:18 +0100

    javadoc

--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
@@ -18,13 +18,23 @@ public class StreetSection extends Entity {
         return false;
     }
 
-    public boolean carCouldEnterNextSection() {
+    /**
+     * Checks, if first car in street section is able to enter the next section, depending on its predefined route.
+     *
+     * @return true = car can enter next section, false = car can not enter next section
+     */
+    public boolean firstCarCouldEnterNextSection() {
         // car knows its route (next section)
 
         // update position in next section
         // if is enough space in next section
         // update pos in previous roundabout section
         // if no car entering from previous roundabout section && enough space in previous section
+
+        if (isFirstCarOnExitPoint()) {
+            return true;
+        }
+
         return false;
     }
 
