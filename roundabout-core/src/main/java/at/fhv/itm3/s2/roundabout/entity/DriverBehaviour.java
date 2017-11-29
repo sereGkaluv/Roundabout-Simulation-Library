3f8229ba1541d8bd3b27da1d06ccef1e5a941544
plitvica <manuela.ljubicic@gmail.com>
2017-11-29 19:06:22 +0100

    DriverBehaviour added

--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/DriverBehaviour.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/DriverBehaviour.java
@@ -3,4 +3,41 @@ package at.fhv.itm3.s2.roundabout.entity;
 import at.fhv.itm3.s2.roundabout.api.entity.IDriverBehaviour;
 
 public class DriverBehaviour implements IDriverBehaviour {
+
+    private double speed;
+    private double minDistanceToNextCar;
+    private double maxDistanceToNextCar;
+    private double behaviourFactor;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getMinDistanceToNextCar() {
+        return minDistanceToNextCar;
+    }
+
+    public void setMinDistanceToNextCar(double minDistanceToNextCar) {
+        this.minDistanceToNextCar = minDistanceToNextCar;
+    }
+
+    public double getMaxDistanceToNextCar() {
+        return maxDistanceToNextCar;
+    }
+
+    public void setMaxDistanceToNextCar(double maxDistanceToNextCar) {
+        this.maxDistanceToNextCar = maxDistanceToNextCar;
+    }
+
+    public double getBehaviourFactor() {
+        return behaviourFactor;
+    }
+
+    public void setBehaviourFactor(double behaviourFactor) {
+        this.behaviourFactor = behaviourFactor;
+    }
 }
