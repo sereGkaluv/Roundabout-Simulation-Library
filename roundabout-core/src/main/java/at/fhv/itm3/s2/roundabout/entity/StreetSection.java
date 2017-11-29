7ca3066b84ae621c26206747cc367cb734cb6161
Mathias Nigsch <mathias.nigsch@gmail.com>
2017-11-29 18:23:28 +0100

    Added "add car" method to street section Added java docs Fixed implementation of street section

--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/entity/StreetSection.java
@@ -1,14 +1,43 @@
 package at.fhv.itm3.s2.roundabout.entity;
 
+import at.fhv.itm3.s2.roundabout.api.entity.ICar;
+import at.fhv.itm3.s2.roundabout.api.entity.IStreetConnector;
 import at.fhv.itm3.s2.roundabout.api.entity.IStreetSection;
 import desmoj.core.simulator.Entity;
 import desmoj.core.simulator.Model;
+
+import java.util.Map;
 
 public class StreetSection extends Entity implements IStreetSection {
 
 
     public StreetSection(Model model, String s, boolean b) {
         super(model, s, b);
+    }
+
+    @Override
+    public double getLengthInMeters() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public IStreetConnector getNextStreetConnector() {
+        return null;
+    }
+
+    @Override
+    public IStreetConnector getPreviousStreetConnector() {
+        return null;
+    }
+
+    @Override
+    public Map<ICar, Double> getCarPositions() {
+        return null;
     }
 
     public void updateAllCarsPositions() {
@@ -24,6 +53,11 @@ public class StreetSection extends Entity implements IStreetSection {
     }
 
     public void moveFirstCarToNextSection() {
+
+    }
+
+    @Override
+    public void addCar(ICar car) {
 
     }
 }
