57eca12c8c7286ea9512417e72956f68ba3960ad
Mathias Nigsch <mathias.nigsch@gmail.com>
2017-11-30 10:51:51 +0100

    Implementation of street connector and definition of interface Added java docs

--- roundabout-api/src/main/java/at/fhv/itm3/s2/roundabout/api/entity/IStreetConnector.java
+++ roundabout-api/src/main/java/at/fhv/itm3/s2/roundabout/api/entity/IStreetConnector.java
@@ -1,4 +1,18 @@
 package at.fhv.itm3.s2.roundabout.api.entity;
 
+import java.util.Set;
+
 public interface IStreetConnector {
+
+    /**
+     * Gets the next connected sections, which are accessible via this connector.
+     * @return The further connected sections.
+     */
+    Set<IStreetSection> getNextSections();
+
+    /**
+     * Gets the previous connected sections, which are accessible through this connector.
+     * @return The previous connected sections.
+     */
+    Set<IStreetSection> getPreviousSections();
 }
