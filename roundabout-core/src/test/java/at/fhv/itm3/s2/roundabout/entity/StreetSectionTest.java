c729b275724d1d7ff9641d93c5533f4ee0176a6c
sereGkaluv <malicx7@gmail.com>
2017-12-13 10:36:55 +0100

    sync with master

--- roundabout-core/src/test/java/at/fhv/itm3/s2/roundabout/entity/StreetSectionTest.java
+++ roundabout-core/src/test/java/at/fhv/itm3/s2/roundabout/entity/StreetSectionTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class StreetSectionTest {
+
     private IStreetSection prepareStreetSectionCarCouldEnterNextSectionMock() {
         IStreetSection streetSectionMock = mock(StreetSection.class);
         when(streetSectionMock.firstCarCouldEnterNextSection()).thenCallRealMethod();
