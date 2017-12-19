54f90c5ea5dd515dc61125429e6c28d32372ee30
daniel <dgr7348@students.fhv.at>
2017-12-19 14:53:43 +0100

    in out on sections

---
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/util/dto/Out.java
@@ -0,0 +1,13 @@
+package at.fhv.itm3.s2.roundabout.util.dto;
+
+public class Out {
+    private String probability;
+
+    public String getProbability() {
+        return probability;
+    }
+
+    public void setProbability(String probability) {
+        this.probability = probability;
+    }
+}
--- roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/util/dto/Sections.java
+++ roundabout-core/src/main/java/at/fhv/itm3/s2/roundabout/util/dto/Sections.java
@@ -3,7 +3,25 @@ package at.fhv.itm3.s2.roundabout.util.dto;
 import java.util.List;
 
 public class Sections {
+    private In in;
+    private Out out;
     private List<Section> sections;
+
+    public In getIn() {
+        return in;
+    }
+
+    public void setIn(In in) {
+        this.in = in;
+    }
+
+    public Out getOut() {
+        return out;
+    }
+
+    public void setOut(Out out) {
+        this.out = out;
+    }
 
     public List<Section> getSections() {
         return sections;
