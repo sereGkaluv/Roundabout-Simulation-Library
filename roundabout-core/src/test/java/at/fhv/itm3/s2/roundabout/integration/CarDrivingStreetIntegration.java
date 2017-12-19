bcba13d4eb39f4a2acc007e9f6c6c0bf5edc0a81
m3rd833r3 <MBayer1@gmx.at>
2017-12-19 21:15:12 +0100

    destinationReached Test

--- roundabout-core/src/test/java/at/fhv/itm3/s2/roundabout/integration/CarDrivingStreetIntegration.java
+++ roundabout-core/src/test/java/at/fhv/itm3/s2/roundabout/integration/CarDrivingStreetIntegration.java
@@ -11,6 +11,7 @@ import at.fhv.itm3.s2.roundabout.mocks.RouteType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +27,39 @@ public class CarDrivingStreetIntegration {
         model.connectToExperiment(exp);
         exp.setShowProgressBar(false);
     }
+
+    @Test
+    public void destinationReached() {
+
+        long start = System.currentTimeMillis();
+        long timeout = 300000; //5 min in milliseconds
+
+        exp.stop(new TimeInstant(60, TimeUnit.SECONDS));
+
+        RouteGenerator routeGenerator = new RouteGenerator(model);
+        IRoute route = routeGenerator.getRoute(RouteType.TWO_STREETSECTIONS);
+        AbstractSource source = route.getSource();
+
+        Street destinationSection = route.getDestinationSection();
+        source.startGeneratingCars();
+        exp.start();
+
+        long current = System.currentTimeMillis();
+        when(destinationSection.getFirstCar().getDestination().getIdentNumber()).thenReturn(Long.valueOf(3));
+
+        while (destinationSection.isEmpty() && current < start + timeout){
+            current = System.currentTimeMillis();
+        }
+
+        if(!destinationSection.isEmpty()){
+            if(destinationSection.getFirstCar() != null) { //Problem: is always empty
+                Assert.assertEquals(destinationSection.getIdentNumber(), destinationSection.getFirstCar().getDestination().getIdentNumber());
+            }
+        }
+        exp.finish();
+        Assert.fail();
+    }
+
 
     @Test
     public void twoStreetSectionsTwoCars_carsShouldEnterSinks() {
