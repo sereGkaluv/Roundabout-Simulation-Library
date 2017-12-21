3f37fb7bf3642e4f1306e8e328d584b5384337ab
daniel <dgr7348@students.fhv.at>
2017-12-21 11:44:53 +0100

    javadoc

--- roundabout-api/src/main/java/at/fhv/itm3/s2/roundabout/api/entity/IRoundaboutStructure.java
+++ roundabout-api/src/main/java/at/fhv/itm3/s2/roundabout/api/entity/IRoundaboutStructure.java
@@ -7,21 +7,67 @@ import java.util.Set;
 
 public interface IRoundaboutStructure {
 
+    /**
+     * Add a street connector to the structure.
+     *
+     * @param streetConnector street connector to be added
+     */
     void addStreetConnector(IStreetConnector streetConnector);
 
+    /**
+     * Add a route to the structure.
+     *
+     * @param route route to be added
+     */
     void addRoute(IRoute route);
 
+    /**
+     * Add a street to the structure.
+     *
+     * @param street street to be added
+     */
     void addStreet(Street street);
 
+    /**
+     * Add a parameter to the structure.
+     *
+     * @param key   key of the parameter
+     * @param value value of the parameter
+     */
     void addParameter(String key, String value);
 
+    /**
+     * Get all street connectors of the structure.
+     *
+     * @return street connectors of structure
+     */
     Set<IStreetConnector> getStreetConnectors();
 
+    /**
+     * Get all routes of the structure.
+     *
+     * @return routes of structure
+     */
     Set<IRoute> getRoutes();
 
+    /**
+     * Get all streets of the structure.
+     *
+     * @return streets of structure
+     */
     Set<Street> getStreets();
 
+    /**
+     * Get all parameters of the structure.
+     *
+     * @return parameters of structure
+     */
     Map<String, String> getParameters();
 
+    /**
+     * Get simulation model of structure.
+     *
+     * @return simulation model of structure
+     */
     Model getModel();
 }
